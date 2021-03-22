05fcc402bf37388419d4ac25465ec81fe825a37c
MarketingPlus <benmarketingplus@gmail.com>
2021-03-22 14:07:58 +1100

    questions added to my js file with the choices and answers following them

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,5 +12,42 @@ const questions = [
         answer: "b. document.getElementById(\"demo\").innerHTML = \"Hello World!\";"
     },
     {
-
+        question: "How do you write \"Hello World\" in an alert box?",
+        choices: ["a. alert(\"Hello World\");", "b. alertBox(\"Hello World\");", "c. msg(\"Hello World\");", "d. msgBox(\"Hello World\");"],
+        answer: "a. alert(\"Hello World\");"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        choices: ["a. if i == 5 then", "b. if i = 5 then", "c. if(i == 5)", "d. if i = 5"],
+        answer: "c. if(i == 5)"
+    },
+    {
+        question: "How to write an IF statement for executing some code if \"i\" is NOT equal to 5?",
+        choices: ["a. if (i <> 5)", "b. if (i != 5)", "c. if i =! 5 then", "d. if i <> 5"],
+        answer: "b. if (i != 5)"
+    },
+    {
+        question: "The first index of an array is ____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        answer: "a. 0"
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
+        answer: "a. onclick"
+    },
+    {
+        question: "How do you call a function named myFunction?",
+        choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
+        answer: "c. myFunctions()"
+    },
+    {
+        question: "How does a FOR loop start?",
+        choices: ["a. for (i = 0; i <= 5)", "b. for i = 1 to 5", "c. for (i <= 5; i++)", "d. for (i = 0; i <= 5; i++)"],
+        answer: "d. for (i = 0; i <= 5; i++)"
+    },
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        choices: ["a. rnd(7.25)", "b. round(7.25)", "c. Math.rnd(7.25)", "d. Math.round(7.25)"],
+        answer: "d. Math.round(7.25)"
     }
