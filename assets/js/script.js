2ce05bfbaca3a5e2dcf582e03e63ae326455dc45
MarketingPlus <benmarketingplus@gmail.com>
2021-03-22 13:59:58 +1100

    creating and adding questions to my JS file

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// adding all my questions
+
+const questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
+        answer: "d. <script>"
+    },
+    {
+        question: "What is the correct JavaScript syntax to change the content of the HTML element below?",
+        choices: ["a. document.getElementByName(\"p\").innerHTML = \"Hello World!\";", "b. document.getElementById(\"demo\").innerHTML = \"Hello World!\";", "c. document.getElement(\"p\").innerHTML = \"Hello World!\";", "d. #demo.innerHTML = \"Hello World!\";"],
+        answer: "b. document.getElementById(\"demo\").innerHTML = \"Hello World!\";"
+    },
+    {
+
+    }
